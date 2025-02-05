77d60d4e0444331a0de50d13329d366d8c8d194d
kazuki-0418 <kazuki.castle0418@gmail.com>
2025-02-05 11:13:07 -0800

    Exercise is done

---
+++ dist/ex03.js
@@ -0,0 +1,26 @@
+"use strict";
+// Exercise: Write a function `logValues` that takes an array of any type of values (numbers, strings, booleans, etc.).
+// The function should iterate through the array and log each value along with its type.
+function logValues(values) {
+    for (const value of values) {
+        if (typeof value === "object") {
+            if (value !== null) {
+                Object.entries(value).forEach(([k, v]) => {
+                    console.log(`{ ${k}: "${v}" } (${typeof value})`);
+                });
+                continue;
+            }
+        }
+        if (typeof value === "string") {
+            console.log(`"${value}" (${typeof value})`);
+            continue;
+        }
+        console.log(`${value} (${typeof value})`);
+    }
+}
+logValues([42, "Hello", true, { name: "Alice" }]);
+// Expected output:
+// 42 (number)
+// "Hello" (string)
+// true (boolean)
+// { name: "Alice" } (object)
--- src/ex03.ts
+++ src/ex03.ts
@@ -1,10 +1,24 @@
 // Exercise: Write a function `logValues` that takes an array of any type of values (numbers, strings, booleans, etc.).
 // The function should iterate through the array and log each value along with its type.
 
-function logValues(values) {
+function logValues(values: unknown[]) {
+  for (const value of values) {
+    if (typeof value === "object") {
+      if (value !== null) {
+        Object.entries(value).forEach(([k, v]) => {
+          console.log(`{ ${k}: "${v}" } (${typeof value})`);
+        });
+        continue;
+      }
+    }
 
+    if (typeof value === "string") {
+      console.log(`"${value}" (${typeof value})`);
+      continue;
+    }
+    console.log(`${value} (${typeof value})`);
+  }
 }
-
 logValues([42, "Hello", true, { name: "Alice" }]);
 // Expected output:
 // 42 (number)
